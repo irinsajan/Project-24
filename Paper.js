139dbfdeaf39b84a3ffde990c390eb4c8caa997d
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-11-22 15:42:18 +0530

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -1,21 +1,22 @@
 class Paper{
-    constructor(x,y,radius,height){
-        this.body = Bodies.rectangle(x,y,radius,height);
-        World.add(world,this.body);
-
-        this.radius = radius;
-        this.height = height;
-    }
-    display(){
-         var pos = this.body.position;
-         var options = {
+    constructor(x,y,radius){
+        var options = {
              isStatic:false,
              restitution:0.3,
              friction:0.5,
              density:1.2
          }
+        this.body = Bodies.rectangle(x,y,radius,options);
+        World.add(world,this.body);
+
+        this.radius = radius;
+    }
+    display(){
+         var pos = this.body.position;
+         
         stroke("black") 
         fill("white");
-        ellipse(pos.x,pos.y,this.radius,this.height);
+        ellipseMode(RADIUS);
+        ellipse(pos.x,pos.y,this.radius,this.radius);
     }
 } 
